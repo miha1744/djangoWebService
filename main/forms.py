fe25da0e822fa65da3c599ed584c7e19ec0e7cf1
Михаил <mihail@MacBook-Pro-Mihail-5.local>
2020-08-20 15:49:11 +0300

    last today push

--- main/forms.py
+++ main/forms.py
@@ -33,16 +33,8 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         # datetime-local is a HTML5 input type, format to make date time show on fields
-        widgets = {
-          'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%d/%m/%y %H:%M'),
-        }
         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        super(EventForm, self).__init__(*args, **kwargs)
-        # input_formats to parse HTML5 datetime-local input to datetime field
-        self.fields['start_time'].input_formats = ('%d/%m/%y %H:%M',)
-        self.fields['end_time'].input_formats = ('%d/%m/%y %H:%M',)
 
 
 
--- main/views.py
+++ main/views.py
@@ -165,16 +165,10 @@ def next_month(day):
 
 def event(request, event_id=None):
     instance = Event()
-    if event_id:
-        instance = get_object_or_404(Event, pk=event_id)
-    else:
-        instance = Event()
+    instance = get_object_or_404(Event, pk=event_id)
 
-    form = EventForm(request.POST or None, instance=instance)
-    if request.POST and form.is_valid():
-        form.save()
-        return redirect('calendar')
-    return render(request, 'event.html', {'form': form})
+    data = {"event": instance}
+    return render(request, 'event.html', context= data)
 
 
 
